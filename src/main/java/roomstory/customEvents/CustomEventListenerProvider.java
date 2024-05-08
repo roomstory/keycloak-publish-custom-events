9d7712603196c82a50a8b0e7e26b47e6a1c2f0ff
Pulkit Chhabra <pulkit.chhabra2@gmail.com>
2024-05-08 19:10:48 +0530

    Adding property file

--- src/main/java/roomstory/customEvents/CustomEventListenerProvider.java
+++ src/main/java/roomstory/customEvents/CustomEventListenerProvider.java
@@ -1,8 +1,12 @@
 package roomstory.customEvents;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Properties;
 
 import org.jboss.logging.Logger;
 import org.keycloak.email.DefaultEmailSenderProvider;
@@ -18,29 +22,35 @@ import org.keycloak.models.UserModel;
 
 public class CustomEventListenerProvider implements EventListenerProvider {
 
-    private static final Logger log = Logger.getLogger(CustomEventListenerProvider.class);
 
+    private static final Logger log = Logger.getLogger(CustomEventListenerProvider.class);
+    
     private final KeycloakSession session;
     private final RealmProvider model;
+    private final Properties properties;
+    private final String nsqHost;
+    private final String nsqPort;
+    private final String operationType;
+    private final String topicName;
 
     public CustomEventListenerProvider(KeycloakSession session) {
         this.session = session;
         this.model = session.realms();
+        this.properties = loadProperties("/config.properties");
+
+        // Initialize other variables
+        this.nsqHost = properties.getProperty("NSQ_HOST");
+        this.nsqPort = properties.getProperty("NSQ_PORT");
+        this.operationType = properties.getProperty("OPERATION_TYPE");
+        this.topicName = properties.getProperty("TOPIC_NAME");
     }
+     
 
     @Override
     public void onEvent(Event event) {
-
-        if (EventType.REGISTER.equals(event.getType())) {
+        if (EventType.REGISTER.equals(event.getType()) || EventType.LOGIN.equals(event.getType()) || EventType.UPDATE_PROFILE.equals(event.getType()) || EventType.REGISTER_ERROR.equals(event.getType())) {
             publishEventToTopic(event);
         }
-        else if(EventType.LOGIN.equals(event.getType())) {
-            publishEventToTopic(event);        
-        }
-        else if(EventType.UPDATE_PROFILE.equals(event.getType())){
-            publishEventToTopic(event);
-        }
-
     }
 
     private void publishEventToTopic(Event event) {
@@ -50,7 +60,7 @@ public class CustomEventListenerProvider implements EventListenerProvider {
         RealmModel realm = this.model.getRealm(event.getRealmId());
         UserModel newRegisteredUser = this.session.users().getUserById(realm, event.getUserId());
 
-        String emailPlainContent = "New user registration\n\n" +
+        String emailPlainContent = event.getType()+ "\n" +
                 "Email: " + newRegisteredUser.getEmail() + "\n" +
                 "Username: " + newRegisteredUser.getUsername() + "\n" +
                 "Client: " + event.getClientId();
@@ -64,9 +74,10 @@ public class CustomEventListenerProvider implements EventListenerProvider {
 
          // DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(session);
 
-        String url = "http://host.docker.internal:4151/pub?topic=test-topic";
+        //String url = "http://host.docker.internal:4151/pub?topic=test-topic";
+        String url = "http://" + nsqHost + ":" + nsqPort+ "/" +operationType + "?topic=" + topicName;
       
-      try {
+        try {
         URL nsqUrl = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) nsqUrl.openConnection();
         conn.setRequestMethod("POST");
@@ -86,6 +97,22 @@ public class CustomEventListenerProvider implements EventListenerProvider {
       }
         log.info("-----------------------------------------------------------");
     }
+
+    private static Properties loadProperties(String configFile) {
+        Properties properties = new Properties();
+        try (InputStream input = CustomEventListenerProvider.class.getResourceAsStream(configFile)) {
+            if (input != null) {
+                properties.load(input);
+            } else {
+                System.err.println("Unable to find config.properties file on the classpath.");
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading config.properties file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
